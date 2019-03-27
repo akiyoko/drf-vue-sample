5e24757941c0823df9b1bec20a276327dc6fd252
akiyoko <akiyoko@users.noreply.github.com>
2019-03-28 00:56:31 +0900

    vue create frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'plugin:vue/base'
+  ],
+  rules: {
+    'no-console': 'off'
+  },
+  parserOptions: {
+     parser: 'babel-eslint',
+     ecmaVersion: 2017,
+     sourceType: 'module'
+  }
+};
